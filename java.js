b5d3d763b33a135f7cfe2e99a21c01475cd52d3b
RhazzXIX <jm21tonio@gmail.com>
2022-12-23 14:18:08 +0800

    Set up initial U.I. structure and styling

--- java.js
+++ java.js
@@ -1,9 +1,5 @@
-const name = "James";
-
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
+(function () {
+  const body = document.querySelector('body');
+  
+  
+}) ();
