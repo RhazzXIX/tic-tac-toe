ec75331c890603f7998298cebcb43e037343715d
RhazzXIX <jm21tonio@gmail.com>
2022-12-26 18:29:54 +0800

    Set up flow for player selections and inputs

--- docs/java.js
+++ docs/java.js
@@ -1,5 +1,6 @@
 const Game = (function () {
   // Add Players //
+
   const players = [];
   const Player = function (name, mark) {
     const player = name;
@@ -17,6 +18,11 @@ const Game = (function () {
 
   const init = function () {
     bindEvents();
+    gameBoard.removeChild(board);
+    gameBoard.removeChild(playerInfo);
+    selectVersus.classList.toggle("hidden");
+    playerInfo.classList.toggle("hidden");
+    board.classList.toggle("hidden");
   };
 
   // cache DOM//
@@ -25,14 +31,64 @@ const Game = (function () {
   const grids = board.querySelectorAll("p.mark");
   const oBtn = gameBoard.querySelector("button#O");
   const xBTn = gameBoard.querySelector("button#X");
-  const selectMarker = gameBoard.querySelector("div#marker");
+  const playerInfo = gameBoard.querySelector("div#info");
   const selectPlayers = gameBoard.querySelector("div#players");
-  const selectVersus = gameBoard.querySelector('div#versus')
+  const selectVersus = gameBoard.querySelector("div#versus");
+  const PVPBtn = gameBoard.querySelector("#vsPlayer");
+  const playerForm = playerInfo.querySelector("form#player-info");
+  const markerO = playerForm.querySelector("button#O");
+  const markerX = playerForm.querySelector("button#X");
+  const nameInput = playerForm.querySelector("input#name");
+  // const listLabel = playerForm.querySelector('label[for=selectList]');
+  const markerList = playerForm.querySelector("ul#selectList");
+  const userLabel = playerForm.querySelector("label#userName");
+  const listMarkerO = markerList.querySelector("li#markerO");
+  const listMarkerX = markerList.querySelector("li#markerX");
 
   const bindEvents = function () {
+    PVPBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      gameBoard.removeChild(selectVersus);
+      gameBoard.appendChild(playerInfo);
+    });
     grids.forEach((grid) => {
       grid.addEventListener("click", playerMarks.bind(grid));
     });
+    playerForm.addEventListener('submit', (e) => {
+      event.preventDefault();
+    })
+    markerO.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (nameInput.value) {
+        inputPlayers(nameInput.value, markerO);
+      }
+    });
+    markerX.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (nameInput.value) {
+        inputPlayers(nameInput.value, markerX);
+      }
+    });
+  };
+
+  const inputPlayers = function (name, marker) {
+    if (players.length === 0) {
+      Player(name, marker);
+      userLabel.textContent = "Player Two:";
+      nameInput.value = "";
+      if (marker.value === "O") {
+        console.log(marker.value);
+        markerList.removeChild(listMarkerO);
+      } else if (marker.value === "X") {
+        console.log(marker.value);
+        markerList.removeChild(listMarkerX);
+      }
+    } else if (players.length === 1) {
+      Player(name, marker);
+      gameBoard.removeChild(playerInfo);
+      gameBoard.appendChild(board);
+      event.preventDefault();
+    }
   };
 
   const playerMarks = function () {
@@ -46,13 +102,7 @@ const Game = (function () {
     console.log(players);
   };
 
-  Player("Next", "X");
-  Player("Roan", "O");
-
   init();
-  gameBoard.removeChild(board);
-  gameBoard.removeChild(selectMarker);
-  gameBoard.removeChild(selectVersus);
 
   return { players, Player, init, bindEvents, playerMarks, playerTurn };
 })();
